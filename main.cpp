f47bf9b8d4f10e888645a8d3ca884d94ee1c557c
juwonBae <rkdmf0000@gmail.com>
2019-08-19 21:39:11 +0900

    CHAR POINTER ARRAY PRACTICE YEEEEEEE

--- main.cpp
+++ main.cpp
@@ -1,31 +1,22 @@
 #include <stdio.h>
 #include <iostream>
-#include <fstream>
-namespace testxxx
-{
-    class exToolClass
-    {
-        public:
-        int fileCreate(std::string txt){
-            std::ofstream file {"hello.txt"};
-            std::string* a = &txt;
-            file << &a;
-        };
-    };
-}
 
-int main()
-{
-    /*
-    파일 생성을 하는데 
-    string을 포인터로 변환시켜서 포인터 변수에 넣고 그걸
-    파일 내용 입력 할 때 역전환 시켜서 넣으면 내용이 그대로 그대로 들어가는지에 대한 
-    테스트
 
-    결괴 : 파일 생성이 안도ㅣ네ㅋㅋㅋ 그냥 문자열을 어디 입력 할 땐 char[] 형태로 하는게 좋을려나.
-    아니면 복호화 가능한 해쉬로 바꿔서 넣는건 불안정해.. 음
-    */
-    testxxx::exToolClass toolInstance = testxxx::exToolClass();
-    std::string dummyText = "edit code on \"visual studio code\" of the best experience.";
-    toolInstance.fileCreate(dummyText);
+
+int main(void)
+{
+
+    //어제부터 char 배열을 막 만지는걸 하고있는데 막 상상한만큼의 결과가 안나오네
+    //출근하기시뤙  
+    char    s[30] = {'f','u','c','k','e','n','e','r','\0','Y','E','E'};
+    int     i;
+    for(i=0;s[i]!='\0';i++) std::cout << s[i];
+
+    std::cout << std::endl;
+
+    char* cp = s;
+
+    std::cout << cp << "==" << &s;
+
+    return 0;
 }
