55fd6dfae388b7ad5f8f5222f3058113ab029f51
juwonBae <rkdmf0000@gmail.com>
2019-08-20 23:54:58 +0900

    yes dog sir man ang ang charlie chilli kitchen

--- history-2019.08/20-function_pointer_example.cpp
+++ history-2019.08/20-function_pointer_example.cpp
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <iostream>
 
-
+//일단 파라미터 명칭 없이 다 쓰갈겨준다
+//이게 그 원형 선언인가 그거구만 이럼 컴파일 에러 안나겠네 굳이 .. 그냥 위에 쓰면 될껄 
+//이래서 해더 파일이 있다고 알고있다.
 double Add(double, double);
 double Sub(double, double);
 double Mul(double, double);
 double Div(double, double);
+//3번짜 파라미터가 함수 포인터로 들어가니까 저리 해준거같다
+//참고로 func는 타입이 아니라 파라미터 이름이다 첨 보고 착각해쓰 ㅎ
 double Calculator(double, double, double (*func)(double, double));
+
 int main(void)
 {
-    double (*calc)(double, double) = NULL; // 함수 포인터 선언
+    //캡슐 닫하기전 내용까진 이해했고 그려려니함
+
+    //이거시 포인터 선언
+    double (*calc)(double, double) = NULL; 
+
+
     double num1 = 3;
     double num2 = 4;
     double result = 0;
@@ -33,9 +43,11 @@ int main(void)
         break;
     }
     result = Calculator(num1, num2, calc);
-    std::cout << "calc " << result << " desu.";
+    std::cout << "calc " << result << " desu. dummy jackies grenner";
     return 0;
 }
+
+//이후에 형식이나 그런것을 다 정의해준다.
 double Add(double num1, double num2) { return num1 + num2; }
 double Sub(double num1, double num2) { return num1 - num2; }
 double Mul(double num1, double num2) { return num1 * num2; }
---
+++ history-2019.08/20.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <iostream>
+
+
+//상수 함수 포인터
+//특징 - 캡슐 안쪽에서 안써짐
+//int (*const jool)();
+
+//비 상수 함수 포인터
+//특징 - 캡슐 안쪽에서 써짐
+int (*alla)();
+
+int dongkey_plus()
+{
+    return 9919;
+}
+
+
+
+//자 오늘은 함수 포인터 라는걸 해볼까
+int main(void)
+{
+    
+    //함수 포인터는 함수로 초기화 가능하다고 한다. 그럼 2개 타입의 포인터를 
+    //쑤셔보자
+    //이런식으로 초기화 한다는데
+    int (*jool)() = dongkey_plus;
+    int (*alla)() = dongkey_plus;
+    
+    //std::cout << jool() << std::endl;
+    std::cout << alla() << std::endl;
+
+
+
+    //비 상수 포인터는 잘 되는데 말야
+    //상수 포인터는 맘대로 잘 안되네 뭔가 더 해야하겠는데
+
+    return 0;
+}
