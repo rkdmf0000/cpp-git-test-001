2a71189787650d465968ba9bcc2d67ce87557096
rkdmf0000 <rkdmf0000@gmail.com>
2019-09-19 22:38:17 +0900

    test

---
+++ loader/load-of-openGL.h
@@ -0,0 +1,16 @@
+//#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+//#pragma message("Link sction loading..")
+#include "c:\MinGW\include\GL\gl.h"
+//#include "c:\MinGW\include\GL\glu.h"
+//#include "c:\Users\my_fl\Desktop\dev-tute\c++\chep1\lib\openGL\3.0.0\include\GL\glut.h"
+//#pragma comment(lib, "c:\Users\my_fl\Desktop\dev-tute\c++\chep1\lib\openGL\msvc-3.0.0\lib\freeglut.lib")
+//#include "c:\Users\my_fl\Desktop\dev-tute\c++\chep1\lib\openGL\msvc-3.0.0\include\GL\glut.h"
+//#if ((ULONG_MAX) == (UINT_MAX))
+//# define IS32BIT
+//#else
+//# define IS64BIT
+//#endif
+//#ifdef IS64BIT
+//#else
+//#endif
