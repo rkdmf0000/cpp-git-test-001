21294e8d944a48ea2b725f253214d79af414b57d
rkdmf0000 <rkdmf0000@gmail.com>
2019-09-19 22:38:05 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-#include "loader\load-of-openGL.cpp"
-
+//#include "loader\load-of-openGL.h"
 #include <stdio.h>
 #include <iostream>
 #include <windows.h>
-
-
+//#include "c:\Users\my_fl\Desktop\dev-tute\c++\chep1\lib\openGL\3.0.0\include\GL\glut.h"
 
 int main(int argc, char **argv)
 {
